25b585f202743b7431aa48b37fd444196491341e
valle <vallukis@gmail.com>
2022-10-26 17:48:56 +0100

    feacture: confecionar el main y llamar a los metodos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Introducir el numero para realizar los calculos");
+        int num = sc.nextInt();
+
+        System.out.println("La suma es: "+Metodo.Suma1aN(num));
+        System.out.println("La multiplicacion es: "+Metodo.Producto1aN(num));
+        System.out.println("La media es: "+Metodo.Media1aN(num));
     }
 }
