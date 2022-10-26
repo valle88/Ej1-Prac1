48da46acc03bfdce9b03c587adc494a775b9fcde
valle <vallukis@gmail.com>
2022-10-26 17:35:17 +0100

    feacture: crear tres metodos para suma,multiplicar,media

---
+++ src/Metodo.java
@@ -0,0 +1,26 @@
+public class Metodo {
+    public static  int Suma1aN(int numero){
+        int resultado = 0;
+        for (int i = 0; i < numero; i++) {
+            resultado += i;
+
+        }
+        return resultado;
+    }
+
+    public static int Producto1aN(int numero){
+        int resultado = 1;
+        for (int i = 2; i < numero; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+
+    public static int Media1aN(int numero){
+        int resultado = 0;
+        for (int i = 0; i <= numero; i++) {
+            resultado += i;
+        }
+        return resultado/numero;
+    }
+}
