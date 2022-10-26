144c5e58abb456e55de5b1ab15f64c8c535ea0f2
valle <vallukis@gmail.com>
2022-10-26 17:50:42 +0100

    feacture: retocar la clase main para que los calculos  salieran bien

--- src/Metodo.java
+++ src/Metodo.java
@@ -1,7 +1,7 @@
 public class Metodo {
     public static  int Suma1aN(int numero){
         int resultado = 0;
-        for (int i = 0; i < numero; i++) {
+        for (int i = 0; i <= numero; i++) {
             resultado += i;
 
         }
@@ -10,7 +10,7 @@ public class Metodo {
 
     public static int Producto1aN(int numero){
         int resultado = 1;
-        for (int i = 2; i < numero; i++) {
+        for (int i = 2; i <= numero; i++) {
             resultado *= i;
         }
         return resultado;
